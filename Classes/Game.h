c4da22222ac5ecc145b764fb95a77122b59fb4c1
Rebecca Emrick <raemrick@gmail.com>
2012-03-09 15:53:48 -0600

    added more methods to Game

--- Classes/Game.h
+++ Classes/Game.h
@@ -25,6 +25,34 @@
 
 //Deal with actions
 -(bool)havingContraction;
+-(bool)isDead; //replace with isHavingCSection?
+
+-(void)giveEpidural;
+-(void)turnOffLights;
+-(void)confineToBed;
+
+
+//A few ways of modeling actions
+
+//list every action
+-(void)giveBackrub;
+-(void)giveIceChips;
+-(void)playMusic;
+-(void)sayMantra; //etc.
+
+//categorize options
+-(void)performFocusingAction;
+-(void)performEnergizingAction;
+-(void)performSupportiveAction;
+//OR
+-(void)performMassage;
+-(void)haveRest;
+-(void)provideDistraction;
+-(void)provideFocus;
+-(void)provideNaturalPainRelief;
+-(void)provideDrugPainRelief;
+
+
 
 
 @end
