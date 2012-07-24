39a1b6eea0ca4ecb34a255118f9ee3e7280f06c9
achmizs <achmizs@gmail>
2012-07-24 14:54:53 -0700

    Contraction model updated, stats update per tick now

---
+++ Digital-Birth/DBSlidingWindowView.h
@@ -0,0 +1,13 @@
+//
+//  DBSlidingWindowView.h
+//  Digital-Birth
+//
+//  Created by Sandy Achmiz on 7/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBSlidingWindowView : UIView
+
+@end
